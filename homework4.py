1d6c2ccaae0f7ad915d42779e0943fa3ca4e934e
toxakzk <Sokolov.a.v.biz@yandex.ru>
2024-05-13 19:25:48 +0300

    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = (1 , 4 , 6 , 'тополь' , True)
+print(immutable_var)
+#immutable_var[3] = 'тюльпан' - кортеж не поддерживает изменения в элементах
+mutable_list = [1 ,3 , 5, 7]
+print(mutable_list)
+mutable_list[0] = 9
+print(mutable_list)
